9a0b32896667bb728df672d71ee47a2debc4062e
Jada Racquel <mccoyjada.9816@gmail.com>
2020-10-02 14:25:07 -0500

    Added window controls to the contact form

---
+++ EContact.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace EContact
+{
+    partial class EContact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EContact));
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.lbl_contactID = new System.Windows.Forms.Label();
+            this.txtbx_contactID = new System.Windows.Forms.TextBox();
+            this.txtbx_contactNum = new System.Windows.Forms.TextBox();
+            this.lbl_contactNum = new System.Windows.Forms.Label();
+            this.txtbx_LName = new System.Windows.Forms.TextBox();
+            this.lbl_LName = new System.Windows.Forms.Label();
+            this.txtbx_fName = new System.Windows.Forms.TextBox();
+            this.lbl_fName = new System.Windows.Forms.Label();
+            this.txtbx_Address = new System.Windows.Forms.TextBox();
+            this.lbl_Address = new System.Windows.Forms.Label();
+            this.lbl_gender = new System.Windows.Forms.Label();
+            this.cmbx_gender = new System.Windows.Forms.ComboBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtbx_search = new System.Windows.Forms.TextBox();
+            this.lbl_search = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.Image")));
+            this.pictureBoxLogo.Location = new System.Drawing.Point(345, 12);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(318, 76);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // lbl_contactID
+            // 
+            this.lbl_contactID.AutoSize = true;
+            this.lbl_contactID.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_contactID.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_contactID.Location = new System.Drawing.Point(48, 123);
+            this.lbl_contactID.Name = "lbl_contactID";
+            this.lbl_contactID.Size = new System.Drawing.Size(92, 20);
+            this.lbl_contactID.TabIndex = 1;
+            this.lbl_contactID.Text = "Contact ID";
+            this.lbl_contactID.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtbx_contactID
+            // 
+            this.txtbx_contactID.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_contactID.Location = new System.Drawing.Point(189, 120);
+            this.txtbx_contactID.Name = "txtbx_contactID";
+            this.txtbx_contactID.Size = new System.Drawing.Size(251, 26);
+            this.txtbx_contactID.TabIndex = 2;
+            // 
+            // txtbx_contactNum
+            // 
+            this.txtbx_contactNum.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_contactNum.Location = new System.Drawing.Point(189, 244);
+            this.txtbx_contactNum.Name = "txtbx_contactNum";
+            this.txtbx_contactNum.Size = new System.Drawing.Size(251, 26);
+            this.txtbx_contactNum.TabIndex = 8;
+            // 
+            // lbl_contactNum
+            // 
+            this.lbl_contactNum.AutoSize = true;
+            this.lbl_contactNum.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_contactNum.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_contactNum.Location = new System.Drawing.Point(48, 247);
+            this.lbl_contactNum.Name = "lbl_contactNum";
+            this.lbl_contactNum.Size = new System.Drawing.Size(99, 20);
+            this.lbl_contactNum.TabIndex = 7;
+            this.lbl_contactNum.Text = "Contact No.";
+            this.lbl_contactNum.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtbx_LName
+            // 
+            this.txtbx_LName.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_LName.Location = new System.Drawing.Point(189, 203);
+            this.txtbx_LName.Name = "txtbx_LName";
+            this.txtbx_LName.Size = new System.Drawing.Size(251, 26);
+            this.txtbx_LName.TabIndex = 6;
+            // 
+            // lbl_LName
+            // 
+            this.lbl_LName.AutoSize = true;
+            this.lbl_LName.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_LName.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_LName.Location = new System.Drawing.Point(48, 206);
+            this.lbl_LName.Name = "lbl_LName";
+            this.lbl_LName.Size = new System.Drawing.Size(89, 20);
+            this.lbl_LName.TabIndex = 5;
+            this.lbl_LName.Text = "Last Name";
+            this.lbl_LName.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtbx_fName
+            // 
+            this.txtbx_fName.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_fName.Location = new System.Drawing.Point(189, 161);
+            this.txtbx_fName.Name = "txtbx_fName";
+            this.txtbx_fName.Size = new System.Drawing.Size(251, 26);
+            this.txtbx_fName.TabIndex = 4;
+            // 
+            // lbl_fName
+            // 
+            this.lbl_fName.AutoSize = true;
+            this.lbl_fName.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_fName.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_fName.Location = new System.Drawing.Point(48, 164);
+            this.lbl_fName.Name = "lbl_fName";
+            this.lbl_fName.Size = new System.Drawing.Size(91, 20);
+            this.lbl_fName.TabIndex = 3;
+            this.lbl_fName.Text = "First Name";
+            this.lbl_fName.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtbx_Address
+            // 
+            this.txtbx_Address.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_Address.Location = new System.Drawing.Point(189, 288);
+            this.txtbx_Address.Multiline = true;
+            this.txtbx_Address.Name = "txtbx_Address";
+            this.txtbx_Address.Size = new System.Drawing.Size(251, 107);
+            this.txtbx_Address.TabIndex = 10;
+            // 
+            // lbl_Address
+            // 
+            this.lbl_Address.AutoSize = true;
+            this.lbl_Address.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Address.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Address.Location = new System.Drawing.Point(48, 288);
+            this.lbl_Address.Name = "lbl_Address";
+            this.lbl_Address.Size = new System.Drawing.Size(74, 20);
+            this.lbl_Address.TabIndex = 9;
+            this.lbl_Address.Text = "Address";
+            this.lbl_Address.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lbl_gender
+            // 
+            this.lbl_gender.AutoSize = true;
+            this.lbl_gender.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_gender.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gender.Location = new System.Drawing.Point(48, 407);
+            this.lbl_gender.Name = "lbl_gender";
+            this.lbl_gender.Size = new System.Drawing.Size(65, 20);
+            this.lbl_gender.TabIndex = 11;
+            this.lbl_gender.Text = "Gender";
+            this.lbl_gender.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // cmbx_gender
+            // 
+            this.cmbx_gender.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbx_gender.FormattingEnabled = true;
+            this.cmbx_gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmbx_gender.Location = new System.Drawing.Point(189, 404);
+            this.cmbx_gender.Name = "cmbx_gender";
+            this.cmbx_gender.Size = new System.Drawing.Size(251, 28);
+            this.cmbx_gender.TabIndex = 12;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.Indigo;
+            this.btn_add.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.ForeColor = System.Drawing.Color.White;
+            this.btn_add.Location = new System.Drawing.Point(179, 477);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(145, 43);
+            this.btn_add.TabIndex = 16;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            // 
+            // btn_update
+            // 
+            this.btn_update.BackColor = System.Drawing.Color.ForestGreen;
+            this.btn_update.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_update.ForeColor = System.Drawing.Color.White;
+            this.btn_update.Location = new System.Drawing.Point(345, 477);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(145, 43);
+            this.btn_update.TabIndex = 17;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = false;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.Red;
+            this.btn_delete.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.ForeColor = System.Drawing.Color.White;
+            this.btn_delete.Location = new System.Drawing.Point(508, 477);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(145, 43);
+            this.btn_delete.TabIndex = 18;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.MediumBlue;
+            this.btn_clear.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.ForeColor = System.Drawing.Color.White;
+            this.btn_clear.Location = new System.Drawing.Point(674, 477);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(145, 43);
+            this.btn_clear.TabIndex = 19;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(491, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(465, 269);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // txtbx_search
+            // 
+            this.txtbx_search.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_search.Location = new System.Drawing.Point(553, 117);
+            this.txtbx_search.Name = "txtbx_search";
+            this.txtbx_search.Size = new System.Drawing.Size(403, 26);
+            this.txtbx_search.TabIndex = 14;
+            // 
+            // lbl_search
+            // 
+            this.lbl_search.AutoSize = true;
+            this.lbl_search.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_search.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_search.Location = new System.Drawing.Point(487, 120);
+            this.lbl_search.Name = "lbl_search";
+            this.lbl_search.Size = new System.Drawing.Size(60, 20);
+            this.lbl_search.TabIndex = 13;
+            this.lbl_search.Text = "Search";
+            this.lbl_search.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(963, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EContact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 546);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtbx_search);
+            this.Controls.Add(this.lbl_search);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.cmbx_gender);
+            this.Controls.Add(this.lbl_gender);
+            this.Controls.Add(this.txtbx_Address);
+            this.Controls.Add(this.lbl_Address);
+            this.Controls.Add(this.txtbx_fName);
+            this.Controls.Add(this.lbl_fName);
+            this.Controls.Add(this.txtbx_LName);
+            this.Controls.Add(this.lbl_LName);
+            this.Controls.Add(this.txtbx_contactNum);
+            this.Controls.Add(this.lbl_contactNum);
+            this.Controls.Add(this.txtbx_contactID);
+            this.Controls.Add(this.lbl_contactID);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EContact";
+            this.Text = "EContact";
+            this.Load += new System.EventHandler(this.EContact_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Label lbl_contactID;
+        private System.Windows.Forms.TextBox txtbx_contactID;
+        private System.Windows.Forms.TextBox txtbx_contactNum;
+        private System.Windows.Forms.Label lbl_contactNum;
+        private System.Windows.Forms.TextBox txtbx_LName;
+        private System.Windows.Forms.Label lbl_LName;
+        private System.Windows.Forms.TextBox txtbx_fName;
+        private System.Windows.Forms.Label lbl_fName;
+        private System.Windows.Forms.TextBox txtbx_Address;
+        private System.Windows.Forms.Label lbl_Address;
+        private System.Windows.Forms.Label lbl_gender;
+        private System.Windows.Forms.ComboBox cmbx_gender;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtbx_search;
+        private System.Windows.Forms.Label lbl_search;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
--- EContact.cs
+++ EContact.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace EContact
 {
-    public partial class Form1 : Form
+    public partial class EContact : Form
     {
-        public Form1()
+        public EContact()
         {
             InitializeComponent();
+        }
+
+        private void EContact_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
